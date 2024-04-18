225955b3cfc49609a33bd0599bfcd517bc71d066
BARBOSA FARIAS Izael <barbosa.f.izael@gmail.com>
2024-04-18 14:02:01 +0200

    modified:   Router/allRoutes.js 	modified:   index.html 	new file:   pages/reservations/allResa.html 	new file:   pages/reservations/reserver.html 	modified:   scss/main.css 	modified:   scss/main.css.map 	modified:   scss/main.scss

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,12 @@ export const allRoutes = [
   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
   new Route("/editPassword", "Mon compte", "/pages/auth/editPassword.html"),
+  new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+  new Route(
+    "/reserver",
+    "Ma réservations",
+    "/pages/reservations/reserver.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
